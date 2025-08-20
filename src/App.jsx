7551c100dc4f684c49e4d7e738e0c5448b3b018b
Yan Yang <y.y.erica@hotmail.com>
2025-08-20 15:36:09 +0200

    add daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import { Route, Routes } from "react-router";
 function App() {
   return (
     <div>
-      <Routes></Routes>
+      <div>
+        <input type="text" className="input input-secondary" />
+        <button className="btn btn-secondary text-2xl">add</button>
+      </div>
     </div>
   );
 }
