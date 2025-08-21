dc5b95e368569d151bbe81f092db75328a1f6306
Marvin Wolff <m.w@nr1.io>
2025-08-21 15:25:01 +0200

    Add Details page with robot information display
    - Created Details.jsx component
    - Added robot details view with image and content sections
    - Implemented update/delete buttons
    - Added navigation back to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router";
 import MainLayout from "./layouts/MainLayout";
 import { Homepage } from "./pages";
+import Details from "./pages/Details";
 import { RobotsContextProvider } from "./contexts/robotsContext";
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Homepage />} />
+          <Route path="robot/:id" element={<Details />} />
         </Route>
       </Routes>
     </RobotsContextProvider>
